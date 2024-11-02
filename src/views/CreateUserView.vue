dcf758656d43299417868c040b6299c9f798c4f9
Nhat1207 <minhnhatnguyenphan1207@gmail.com>
2024-11-02 14:03:05 +0700

    add logic validate for form

--- src/views/CreateUserView.vue
+++ src/views/CreateUserView.vue
@@ -5,11 +5,15 @@
         <form @submit.prevent="submit()">
             <div class="fullName">
                 <label>Họ và tên:</label>
-                <input type="text" v-model="fullName"/>
+                <input type="text" v-model="fullName" @input="validateFullName()" />
+                <p v-if="fullNameError" class="error">{{fullNameError}}</p>
+                <p v-if="fullNameSuccess" class="success">{{ fullNameSuccess }}</p>
             </div>
             <div class="phoneNum">
                 <label>Số điện thoại:</label>
-                <input type="tel" v-model="phoneNum"/>
+                <input type="tel" v-model="phoneNum" @input="validatePhoneNum()"/>
+                <p v-if="phoneNumError" class="error">{{phoneNumError}}</p>
+                <p v-if="phoneNumSuccess" class="success">{{ phoneNumSuccess }}</p>
             </div>
             <div class="skill">
                 <label>Kỹ năng:</label>
@@ -32,9 +36,42 @@
 import { ref } from 'vue'
 const fullName= ref("")
 const phoneNum= ref("")
+const fullNameError= ref('')
+const fullNameSuccess= ref('')
+const phoneNumError= ref('')
+const phoneNumSuccess= ref('')
 const submit= () =>{
     if(fullName.value!=""&& phoneNum.value!=""){
     alert('Đăng ký thành công')
+    }
+}
+const validateFullName= ()=>{
+    if(fullName.value == ""){
+        fullNameError.value="Không được bỏ trống"
+        fullNameSuccess.value=""
+    }else if(fullName.value.length < 8){
+        fullNameError.value="Bạn phải nhập tên đủ 8 ký tự"
+        fullNameSuccess.value=""
+    }else{
+        fullNameError.value=""
+        fullNameSuccess.value="Hợp lệ"
+    }
+}
+const validatePhoneNum= ()=>{
+    const numRegex= /[0-9]/
+    if(phoneNum.value == ""){
+        phoneNumError.value="Không được bỏ trống"
+        phoneNumSuccess.value=""
+    }else if(phoneNum.value.length>10){
+        phoneNumError.value="Chỉ nhập 10 số"
+        phoneNumSuccess.value=""
+    }else if(!numRegex.test(phoneNum.value)){
+         phoneNumError.value="Chỉ được nhập số"
+        phoneNumSuccess.value=""
+    }
+    else{
+        phoneNumError.value=""
+        phoneNumSuccess.value="Hợp lệ"
     }
 }
 </script>
@@ -71,5 +108,11 @@ input[type="text"],
 input[type="tel"],
 select{
     float: right;
+}
+.error{
+    color: red;
+}
+.success{
+    color: green;
 }
 </style>
